b8908aa4a01ea5f00e2f6ea3ed9a4617ca0cf480
Cyprien Ménard <cyprien.menard@reseau.eseo.fr>
2024-09-30 14:05:58 +0200

    Import project

---
+++ app/main.c
@@ -0,0 +1,85 @@
+/**
+ *******************************************************************************
+ * @file 	main.c
+ * @author 	jjo
+ * @date 	Mar 29, 2024
+ * @brief	Fichier principal de votre projet sur carte Nucléo STM32G431KB
+ *******************************************************************************
+ */
+
+#include "config.h"
+#include "stm32g4_sys.h"
+
+#include "stm32g4_systick.h"
+#include "stm32g4_gpio.h"
+#include "stm32g4_uart.h"
+#include "stm32g4_utils.h"
+
+#include <stdio.h>
+
+void write_LED(bool b)
+{
+	HAL_GPIO_WritePin(LED_GREEN_GPIO, LED_GREEN_PIN, b);
+}
+
+bool char_received(uart_id_t uart_id)
+{
+	if( BSP_UART_data_ready(uart_id) )	/* Si un caractère est reçu sur l'UART 2*/
+	{
+		/* On "utilise" le caractère pour vider le buffer de réception */
+		BSP_UART_get_next_byte(uart_id);
+		return true;
+	}
+	else
+		return false;
+}
+
+static volatile uint32_t t = 0;
+
+void process_ms(void)
+{
+	if(t)
+		t--;
+}
+
+
+/**
+  * @brief  Point d'entrée de votre application
+  */
+int main(void)
+{
+	/* Cette ligne doit rester la première de votre main !
+	 * Elle permet d'initialiser toutes les couches basses des drivers (Hardware Abstraction Layer),
+	 * condition préalable indispensable à l'exécution des lignes suivantes.
+	 */
+	HAL_Init();
+
+	/* Initialisation des périphériques utilisés dans votre programme */
+	BSP_GPIO_enable();
+	BSP_UART_init(UART2_ID,115200);
+
+	/* Indique que les printf sont dirigés vers l'UART2 */
+	BSP_SYS_set_std_usart(UART2_ID, UART2_ID, UART2_ID);
+
+	/* Initialisation du port de la led Verte (carte Nucleo) */
+	BSP_GPIO_pin_config(LED_GREEN_GPIO, LED_GREEN_PIN, GPIO_MODE_OUTPUT_PP,GPIO_NOPULL,GPIO_SPEED_FREQ_HIGH,GPIO_NO_AF);
+
+	/* On ajoute la fonction process_ms à la liste des fonctions appelées automatiquement
+	 * chaque ms par la routine d'interruption du périphérique Systick */
+	BSP_systick_add_callback_function(&process_ms);
+
+	/* Hello student */
+	printf("Hi <Student>, can you read me?\n");
+
+	/* Tâche de fond, boucle infinie, Infinite loop,... quelque soit son nom vous n'en sortirez jamais */
+	while (1)
+	{
+		if( char_received(UART2_ID) )
+		{
+			t=100;	/* t? Faites un ctrl+clic dessus pour voir... */
+			write_LED(true);	/* ... ça fonctionne aussi avec les macros, les fonctions. C'est votre nouveau meilleur ami */
+			while(t);
+			write_LED(false);
+		}
+	}
+}
---
+++ drivers/bsp/MatrixKeyboard/stm32g4_matrix_keyboard.h
@@ -0,0 +1,54 @@
+/**
+ *******************************************************************************
+ * @file	stm32g4_matrix_keyboard.h
+ * @author	vchav
+ * @date	Jun 10, 2024
+ * @brief	Module pour utiliser un clavier matriciel (adaptation du module de Samuel Poiraud sur f103)
+ *******************************************************************************
+ */
+
+#ifndef BSP_MATRIXKEYBOARD_STM32G4_MATRIX_KEYBOARD_H_
+#define BSP_MATRIXKEYBOARD_STM32G4_MATRIX_KEYBOARD_H_
+
+#include "config.h"
+#if USE_MATRIX_KEYBOARD
+#include <stdbool.h>
+
+
+#define PORT_INPUT_0 	GPIOA
+#define PORT_INPUT_1 	GPIOA
+#define PORT_INPUT_2 	GPIOA
+#define PORT_INPUT_3 	GPIOB
+#define	PIN_INPUT_0		GPIO_PIN_9
+#define	PIN_INPUT_1		GPIO_PIN_10
+#define	PIN_INPUT_2		GPIO_PIN_12
+#define	PIN_INPUT_3		GPIO_PIN_0
+
+#define PORT_OUTPUT_0 	GPIOB
+#define PORT_OUTPUT_1 	GPIOA
+#define PORT_OUTPUT_2 	GPIOA
+#define PORT_OUTPUT_3 	GPIOA
+#define	PIN_OUTPUT_0	GPIO_PIN_7
+#define	PIN_OUTPUT_1	GPIO_PIN_15
+#define	PIN_OUTPUT_2	GPIO_PIN_1
+#define	PIN_OUTPUT_3	GPIO_PIN_0
+
+#define MATRIX_KEYBOARD_HAL_CLOCK_ENABLE()	__HAL_RCC_GPIOA_CLK_ENABLE()
+
+#define CONFIG_PULL_UP
+//#define CONFIG_PULL_DOWN
+
+
+#define NO_KEY 		0xFF
+#define MANY_KEYS 	0xFE
+
+
+void BSP_MATRIX_KEYBOARD_init(const char * new_keyboard_keys);
+void BSP_MATRIX_KEYBOARD_demo_process_main(void);
+void BSP_MATRIX_KEYBOARD_demo_process_1ms(void);
+bool BSP_MATRIX_KEYBOARD_is_pressed(void);
+char BSP_MATRIX_KEYBOARD_get_key(void);
+void BSP_MATRIX_KEYBOARD_press_and_release_events(char * press_event, char * release_event, uint32_t * all_touchs_pressed);
+
+#endif
+#endif /* BSP_MATRIXKEYBOARD_STM32G4_MATRIX_KEYBOARD_H_ */
